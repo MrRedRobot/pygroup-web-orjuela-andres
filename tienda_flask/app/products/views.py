bc3ce41a7a23600a08073edb930fc3870e73e0c5
David_Orjuela <42826331+MrRedRobot@users.noreply.github.com>
2020-11-06 00:55:36 -0500

    Update views.py

--- tienda_flask/app/products/views.py
+++ tienda_flask/app/products/views.py
@@ -10,3 +10,7 @@ def get_products(name):
 
 def render_template():
     return None
+
+#Flask proporciona una función llamada render_template()para renderizar Plantillas. Es esta función la que integra Jinja con Flask. 
+#Para renderizar una plantilla, llamamos render_template()con el nombre de la plantilla junto con los datos que desea pasar a la plantilla como argumentos de palabras clave. La render_template()función representa la plantilla y devuelve HTML como una cadena. 
+#Los argumentos de palabras clave que pasamos a la plantilla se conocen como contexto de la plantilla o simplemente contexto de plantilla.
